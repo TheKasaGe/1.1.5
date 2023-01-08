05b165ae975d413b4294ee0c0073908b1e08b314
TheKasaGe <118396007+TheKasaGe@users.noreply.github.com>
2023-01-08 12:00:31 +0300

    Внедрение дао в сервис через поле

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -2,39 +2,33 @@ package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    private final UserDao dao = new UserDaoHibernateImpl();
     public void createUsersTable() {
-        UserDao dao = new UserDaoHibernateImpl();
         dao.createUsersTable();
     }
 
     public void dropUsersTable() {
-        UserDao dao = new UserDaoHibernateImpl();
         dao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        UserDao dao = new UserDaoHibernateImpl();
         dao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        UserDao dao = new UserDaoHibernateImpl();
         dao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        UserDao dao = new UserDaoHibernateImpl();
         return dao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        UserDao dao = new UserDaoHibernateImpl();
         dao.cleanUsersTable();
     }
 }
